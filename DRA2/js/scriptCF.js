3825f8d9ab2fa39c347428ed2f66dc193ce2bfd4
TakeiNoteuku456 <caua.alves@aluno.ifsp.edu.br>
2024-04-14 10:55:38 -0300

    Adicionadas novas funcionalidades ao projeto

--- DRA2/js/scriptCF.js
+++ DRA2/js/scriptCF.js
@@ -1,7 +1,11 @@
-// Função para lidar com o envio do formulário
 document.getElementById("cadastroForm").addEventListener("submit", function(event) {
     event.preventDefault(); // Evita o envio padrão do formulário
-
-    // Redireciona para a página do administrador
-    window.location.href = "PaginaAdministrador.html";
+    
+    // Exibe a mensagem de sucesso
+    document.getElementById("successMessage").style.display = "block";
+    
+    // Redireciona para a página do administrador após 3 segundos (3000 milissegundos)
+    setTimeout(function() {
+        window.location.href = "PaginaAdministrador.html";
+    }, 3000);
 });
